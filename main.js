514e166868738d897a763114d0da498a7e203fa4
CACHIA Killian <killian.cch@gmail.com>
2023-03-22 21:23:35 +0100

    Page d'accueil, navbar, introduction

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-import './style.css'
+import './style.css';
+import './assets/fontawesome-free-6.3.0-web/css/all.min.css';
+import './assets/fontawesome-free-6.3.0-web/js/all.min.js';
+import 'bootstrap/scss/bootstrap.scss';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
